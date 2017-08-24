ce412f176c19581aeb9420027667d509df6650bd
djansyle <djansyledjans@gmail.com>
2017-08-24 22:51:37 +0800

    Initial Commit

---
+++ entrypoint.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+set -eo pipefail
+
+if [ ! -f /.decrypted ]
+then
+    sourceDir="$SOURCE_DIR"
+
+    if [ -z "$PASSWORD" ]
+    then
+        echo "Missing PASSWORD environment variable."
+        exit 1
+    fi
+
+    password="$PASSWORD"
+    if [ -z "$SOURCE_DIR" ]
+    then
+        sourceDir="/usr/src/app"
+        echo "Setting to default secured dir to ${sourceDir}"
+    fi
+
+    openssl enc -d -aes-256-cbc -in /files.db -out /files.zip -k ${password}
+    mkdir -p ${sourceDir}
+    unzip /files.zip -d ${sourceDir}
+
+    rm /files.db /files.zip
+
+    touch .decrypted
+fi
+exec "$@"
